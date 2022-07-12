f77e0539ceb5e348e35bbad2ea77a715255a92fc
BakayAlexander <bakay.dvr@gmail.com>
2022-07-12 10:13:18 +0300

    fix linter errors

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,9 +1,8 @@
 import { useSelector } from 'react-redux';
-import { selectAllPosts, getPostsStatus, getPostsError, selectPostIds } from './postsSlice';
+import {  getPostsStatus, getPostsError, selectPostIds } from './postsSlice';
 import PostsExcerpt from './PostsExcerpt';
 
 const PostsList = () => {
-	// const posts = useSelector(selectAllPosts);
 	const orderedPostIds = useSelector(selectPostIds);
 	const postStatus = useSelector(getPostsStatus);
 	const error = useSelector(getPostsError);
@@ -12,8 +11,6 @@ const PostsList = () => {
 	if (postStatus === 'loading') {
 		content = <p>"Loading..."</p>;
 	} else if (postStatus === 'succeeded') {
-		// const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date));
-		// content = orderedPosts.map((post) => <PostsExcerpt key={post.id} post={post} />);
 		content = orderedPostIds.map((postId) => <PostsExcerpt key={postId} postId={postId} />);
 	} else if (postStatus === 'failed') {
 		content = <p>{error}</p>;
